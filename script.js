b96af937e34e72e20c7381fd67d299a9ba49d10d
SupremeBeing <105876513+RudraAyachit@users.noreply.github.com>
2025-01-30 16:09:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,290 +1,420 @@
 // Example leaderboard data
 let leaderboard = [
-  { name:"Nalini",
-   wpm: 37,
-   
-    name: "Piyush",
-    wpm: 122
+  {
+    "name": "Piyush",
+    "wpm": 122
+  },
+  {
+    "name": "Adeeb",
+    "wpm": 119
+  },
+  {
+    "name": "Bharadwaj",
+    "wpm": 105
+  },
+  {
+    "name": "Vinmai",
+    "wpm": 102
+  },
+  {
+    "name": "Ryan",
+    "wpm": 101
+  },
+  {
+    "name": "Sam",
+    "wpm": 100
+  },
+  {
+    "name": "Asad",
+    "wpm": 96
+  },
+  {
+    "name": "Ayaan",
+    "wpm": 93
+  },
+  {
+    "name": "Harsh Manav",
+    "wpm": 93
+  },
+  {
+    "name": "Maneeth",
+    "wpm": 87
+  },
+  {
+    "name": "Akshith",
+    "wpm": 81
+  },
+  {
+    "name": "Mary",
+    "wpm": 78
+  },
+  {
+    "name": "Sathwik",
+    "wpm": 76
+  },
+  {
+    "name": "Dr Michelle",
+    "wpm": 75
+  },
+  {
+    "name": "Chandu",
+    "wpm": 71
+  },
+  {
+    "name": "Shravani",
+    "wpm": 70
+  },
+  {
+    "name": "Akhil",
+    "wpm": 69
+  },
+  {
+    "name": "Prem",
+    "wpm": 67
+  },
+  {
+    "name": "Aditya",
+    "wpm": 66
+  },
+  {
+    "name": "Bhuvan",
+    "wpm": 64
+  },
+  {
+    "name": "Nandu",
+    "wpm": 64
+  },
+  {
+    "name": "Janith",
+    "wpm": 63
+  },
+  {
+    "name": "Adip",
+    "wpm": 63
+  },
+  {
+    "name": "Pavithra",
+    "wpm": 63
+  },
+  {
+    "name": "Dhruval",
+    "wpm": 62
+  },
+  {
+    "name": "Sudarshan",
+    "wpm": 62
+  },
+  {
+    "name": "Anish",
+    "wpm": 62
+  },
+  {
+    "name": "Samanyu",
+    "wpm": 59
+  },
+  {
+    "name": "Pepluis Sir",
+    "wpm": 58
+  },
+  {
+    "name": "Cynthia",
+    "wpm": 57
+  },
+  {
+    "name": "Kaushik",
+    "wpm": 57
+  },
+  {
+    "name": "Dinesh",
+    "wpm": 57
+  },
+  {
+    "name": "Priya",
+    "wpm": 57
+  },
+  {
+    "name": "Riya Mam",
+    "wpm": 57
   },
   {
-    name: "Adeeb",
-    wpm: 119
+    "name": "Jaash",
+    "wpm": 56
   },
   {
-    name: "Bharadwaj",
-    wpm: 105
+    "name": "Gagan",
+    "wpm": 56
   },
   {
-    name: "Vinmai",
-    wpm: 102
+    "name": "Hassini",
+    "wpm": 56
   },
   {
-    name: "Ryan",
-    wpm: 101
+    "name": "Sarvani",
+    "wpm": 55
   },
   {
-    name: "Sam",
-    wpm: 100
+    "name": "Nihanth V",
+    "wpm": 55
   },
   {
-    name: "Asad",
-    wpm: 96
+    "name": "Sai Satwik",
+    "wpm": 55
   },
   {
-    name: "Ayaan",
-    wpm: 93
+    "name": "Geetika",
+    "wpm": 55
   },
   {
-    name: "Harsh Manav",
-    wpm: 93
+    "name": "Sai Ganesha",
+    "wpm": 55
   },
   {
-    name: "Maneeth",
-    wpm: 87
+    "name": "Vaishnavi",
+    "wpm": 54
   },
   {
-    name: "Mary",
-    wpm: 78
+    "name": "Suhas N",
+    "wpm": 53
   },
   {
-    name: "Sathwik",
-    wpm: 76
+    "name": "Chidvilas M",
+    "wpm": 52
   },
   {
-    name: "Dr Michelle",
-    wpm: 75
+    "name": "Nishnath",
+    "wpm": 52
   },
   {
-    name: "Chandu",
-    wpm: 71
+    "name": "Arjun",
+    "wpm": 52
   },
   {
-    name: "Akhil",
-    wpm: 69
+    "name": "Sumanth",
+    "wpm": 52
   },
   {
-    name: "Prem",
-    wpm: 67
+    "name": "Deepti K",
+    "wpm": 51
   },
   {
-    name: "Nandu",
-    wpm: 64
+    "name": "Hanish",
+    "wpm": 51
   },
   {
-    name: "Pavithra",
-    wpm: 63
+    "name": "Suhas",
+    "wpm": 51
   },
   {
-    name: "Sudarshan",
-    wpm: 62
+    "name": "Diya",
+    "wpm": 51
   },
   {
-    name: "Anish",
-    wpm: 62
+    "name": "Manish Reddy",
+    "wpm": 50
   },
   {
-    name: "Samanyu",
-    wpm: 59
+    "name": "Deepthi",
+    "wpm": 50
   },
   {
-    name: "Pepluis Sir",
-    wpm: 58
+    "name": "Jahnavi",
+    "wpm": 50
   },
   {
-    name: "Kaushik",
-    wpm: 57
+    "name": "Dakshina",
+    "wpm": 50
   },
   {
-    name: "Dinesh",
-    wpm: 57
+    "name": "Deepak",
+    "wpm": 50
   },
   {
-    name: "Priya",
-    wpm: 57
+    "name": "Rakshit",
+    "wpm": 49
   },
   {
-    name: "Riya Mam",
-    wpm: 57
+    "name": "Mohana",
+    "wpm": 49
   },
   {
-    name: "Gagan",
-    wpm: 56
+    "name": "Deeksha",
+    "wpm": 48
   },
   {
-    name: "Hassini",
-    wpm: 56
+    "name": "Millenium",
+    "wpm": 48
   },
   {
-    name: "Geetika",
-    wpm: 55
+    "name": "Nihal",
+    "wpm": 48
   },
   {
-    name: "Sai Ganesha",
-    wpm: 55
+    "name": "Palvasha",
+    "wpm": 48
   },
   {
-    name: "Vaishnavi",
-    wpm: 54
+    "name": "Sai Daiwik",
+    "wpm": 46
   },
   {
-    name: "Rahul",
-    wpm: 54
+    "name": "Gayatri",
+    "wpm": 46
   },
   {
-    name: "Nishnath",
-    wpm: 52
+    "name": "M B Sneha",
+    "wpm": 45
   },
   {
-    name: "Arjun",
-    wpm: 52
+    "name": "Leena",
+    "wpm": 44
   },
   {
-    name: "Sumanth",
-    wpm: 52
+    "name": "Nirmal",
+    "wpm": 44
   },
   {
-    name: "Suhas",
-    wpm: 51
+    "name": "Anchal",
+    "wpm": 43
   },
   {
-    name: "Diya",
-    wpm: 51
+    "name": "Manish",
+    "wpm": 43
   },
   {
-    name: "Jahnavi",
-    wpm: 50
+    "name": "Wilson",
+    "wpm": 43
   },
   {
-    name: "Dakshina",
-    wpm: 50
+    "name": "Rahul",
+    "wpm": 42
   },
   {
-    name: "Deepak",
-    wpm: 50
+    "name": "Sannihith",
+    "wpm": 40
   },
   {
-    name: "Mohana",
-    wpm: 49
+    "name": "Manasa",
+    "wpm": 39
   },
   {
-    name: "Palvasha",
-    wpm: 48
+    "name": "Satya Vaishnavi",
+    "wpm": 39
   },
   {
-    name: "Hanish",
-    wpm: 45
+    "name": "Santhosh Sir",
+    "wpm": 39
   },
   {
-    name: "Leena",
-    wpm: 44
+    "name": "Nitin Sai",
+    "wpm": 38
   },
   {
-    name: "Nirmal",
-    wpm: 44
+    "name": "Tanvi",
+    "wpm": 38
   },
   {
-    name: "Anchal",
-    wpm: 43
+    "name": "Sumith",
+    "wpm": 38
   },
   {
-    name: "Manish",
-    wpm: 43
+    "name": "Yashaswini",
+    "wpm": 38
   },
   {
-    name: "Wilson",
-    wpm: 43
+    "name": "Musharraf",
+    "wpm": 37
   },
   {
-    name: "Anchal",
-    wpm: 43
+    "name": "Ashar Khan",
+    "wpm": 37
   },
   {
-    name: "Sumith",
-    wpm: 38
+    "name": "Dr. Anand Kakarla",
+    "wpm": 36
   },
   {
-    name: "Yashaswini",
-    wpm: 38
+    "name": "Noorla Sheikh",
+    "wpm": 35
   },
   {
-    name: "Ashar Khan",
-    wpm: 37
+    "name": "Kailash",
+    "wpm": 34
   },
   {
-    name: "Noorla Sheikh",
-    wpm: 35
+    "name": "Vridhi",
+    "wpm": 33
   },
   {
-    name: "Kailash",
-    wpm: 34
+    "name": "Ayman",
+    "wpm": 32
   },
   {
-    name: "Navatej",
-    wpm: 32
+    "name": "Navatej",
+    "wpm": 32
   },
   {
-    name: "Praveena",
-    wpm: 30
+    "name": "Vaishnavi",
+    "wpm": 31
   },
   {
-    name: "Vishnu",
-    wpm: 29
+    "name": "Praveena",
+    "wpm": 30
   },
   {
-    name: "Ajith Sir",
-    wpm: 29
+    "name": "Vishnu",
+    "wpm": 29
   },
   {
-    name: "Ajith Sir",
-    wpm: 29
+    "name": "Ajith Sir",
+    "wpm": 29
   },
   {
-    name: "Shivamani",
-    wpm: 28
+    "name": "Dr. Suman Chirra",
+    "wpm": 28
   },
   {
-    name: "Yashwanth",
-    wpm: 28
+    "name": "Shivamani",
+    "wpm": 28
   },
   {
-    name: "Shivamani",
-    wpm: 28
+    "name": "Yashwanth",
+    "wpm": 28
   },
   {
-    name: "Rounak",
-    wpm: 28
+    "name": "Rounak",
+    "wpm": 28
   },
   {
-    name: "Gagan",
-    wpm: 26
+    "name": "Ram",
+    "wpm": 27
   },
   {
-    name: "Kesava",
-    wpm: 24
+    "name": "Zaryaan",
+    "wpm": 25
   },
   {
-    name: "Anirudh",
-    wpm: 24
+    "name": "Kesava",
+    "wpm": 24
   },
   {
-    name: "Anirudh",
-    wpm: 24
+    "name": "Anirudh",
+    "wpm": 24
   },
   {
-    name: "Kesava",
-    wpm: 24
+    "name": "Srinivas",
+    "wpm": 23
   },
   {
-    name: "Srinivas",
-    wpm: 23
+    "name": "Ramesh",
+    "wpm": 21
   },
   {
-    name: "Ramesh",
-    wpm: 21
+    "name": "Vyshnavi",
+    "wpm": 20
   },
   {
-    name: "Meher Gayatri Mam",
-    wpm: 12
+    "name": "Meher Gayatri Mam",
+    "wpm": 32
   }
 ];
 
